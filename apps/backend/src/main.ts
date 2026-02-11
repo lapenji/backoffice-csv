76dddf96973b5b0192db842b6bde6e0272a267cb
lapenji <laptomb@gmail.com>
2026-02-11 09:29:56 +0100

    feat(backend): added allowcors and changed port

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -21,7 +21,11 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
