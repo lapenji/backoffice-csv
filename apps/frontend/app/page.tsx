56192f32f9b3c5e1515583e8e3b4b231515ab18e
lapenji <laptomb@gmail.com>
2026-02-11 09:42:34 +0100

    feat(frontend): made first page to test fetch

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,3 +1,32 @@
+"use client";
+
+import { getProducts } from "@/lib/api/products";
+import { useState } from "react";
+import useSWR from "swr";
+
 export default function Home() {
-  return <div></div>;
+  const [page, setPage] = useState(1);
+  const limit = 10;
+  const { data, error, isLoading } = useSWR(
+    ["products", page],
+    () => getProducts(page, limit),
+    {
+      keepPreviousData: true,
+    },
+  );
+
+  if (error) {
+    console.log("ERRORE", error);
+    return <p>errore</p>;
+  }
+  if (isLoading) {
+    return <p>loading...</p>;
+  }
+  return (
+    <div>
+      {data?.data.map((p) => (
+        <h1>{p.name}</h1>
+      ))}
+    </div>
+  );
 }
